938703a6dd7ae920bedf09f10658a7f352c11444
Abiria <abiria.dev@gmail.com>
2024-03-19 16:32:33 +0900

    feat: define basic layout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,44 @@
+import {
+	AppBar,
+	Stack,
+	Unstable_Grid2 as Grid,
+} from '@mui/material'
 import { PostPreview } from './components/PostPreview'
 
 function App() {
 	return (
 		<>
 			<header>
-				<h1>Coding Lab</h1>
+				<AppBar></AppBar>
 			</header>
-			<main>
-				<PostPreview />
-			</main>
+			<Grid container spacing={2}>
+				<Grid xs></Grid>
+				<Grid xs={8}>
+					<main className="w-full">
+						<Stack
+							className="w-full"
+							spacing={2}
+						>
+							<PostPreview
+								title="Title"
+								content="content"
+								stretch
+							/>
+							<PostPreview
+								title="Title"
+								content="content"
+								stretch
+							/>
+							<PostPreview
+								title="Title"
+								content="content"
+								stretch
+							/>
+						</Stack>
+					</main>
+				</Grid>
+				<Grid xs></Grid>
+			</Grid>
 		</>
 	)
 }
