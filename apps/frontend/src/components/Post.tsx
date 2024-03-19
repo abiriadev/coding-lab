c70253029b92439bb34f43ed20fe0a81eddc0ffa
Abiria <abiria.dev@gmail.com>
2024-03-19 17:01:37 +0900

    feat: define props and add title, content props

--- apps/frontend/src/components/Post.tsx
+++ apps/frontend/src/components/Post.tsx
@@ -1,12 +1,25 @@
-import { Unstable_Grid2 as Grid } from '@mui/material'
+import {
+	Unstable_Grid2 as Grid,
+	Button,
+	Stack,
+} from '@mui/material'
 
-export const Post = () => {
+export interface PostProps {
+	title: string
+	content: string
+}
+
+export const Post = ({ title, content }: PostProps) => {
 	return (
 		<Grid container>
 			<Grid xs></Grid>
 			<Grid xs={8}>
-				<main></main>
-				<div className="comments"></div>
+				<h1>{title}</h1>
+				<main>{content}</main>
+				<div className="comments">
+					<Button>Add new comment</Button>
+					<Stack></Stack>
+				</div>
 			</Grid>
 			<Grid xs></Grid>
 		</Grid>
