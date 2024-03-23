686368128804012f0d0b7953c55454b04dc1c80d
Abiria <abiria.dev@gmail.com>
2024-03-23 15:35:59 +0900

    feat: show upvotes and downvotes

--- apps/frontend/src/components/Comment.tsx
+++ apps/frontend/src/components/Comment.tsx
@@ -1,14 +1,22 @@
-import { Avatar, Stack } from '@mui/material'
+import { Avatar, Button, Stack } from '@mui/material'
 import { RelativeTimestamp } from './RelativeTimestamp'
+import {
+	ThumbDownOutlined,
+	ThumbUpOutlined,
+} from '@mui/icons-material'
 
 export interface CommentProps {
 	content: string
 	postedAt?: Date
+	upvotes?: number
+	downvotes?: number
 }
 
 export const Comment = ({
 	content,
 	postedAt,
+	upvotes,
+	downvotes,
 }: CommentProps) => {
 	return (
 		<div className="flex gap-3 p-4">
@@ -32,6 +40,26 @@ export const Comment = ({
 					/>
 				</Stack>
 				<div className="text-14">{content}</div>
+				<Stack
+					className="mt-2"
+					direction="row"
+					spacing={2}
+				>
+					<Button
+						color="inherit"
+						startIcon={<ThumbUpOutlined />}
+						className="hover:rounded-full"
+					>
+						{upvotes ?? 0}
+					</Button>
+					<Button
+						color="inherit"
+						startIcon={<ThumbDownOutlined />}
+						className="hover:rounded-full"
+					>
+						{downvotes ?? 0}
+					</Button>
+				</Stack>
 			</div>
 		</div>
 	)
