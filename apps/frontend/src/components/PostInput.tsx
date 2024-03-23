424e665e747c0a610324015ce0e3014af5983b92
Abiria <abiria.dev@gmail.com>
2024-03-24 03:44:31 +0900

    feat: give a gap and min height

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -3,9 +3,9 @@ import { FlatButton } from './FlatButton'
 
 export const PostInput = () => {
 	return (
-		<div>
+		<div className="flex flex-col gap-3">
 			<TextField></TextField>
-			<TextareaAutosize className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-[1.25em] p-5 text-lg" />
+			<TextareaAutosize className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-[1.25em] p-5 text-lg" />
 			<div className="flex justify-end gap-2">
 				<FlatButton>Cancel</FlatButton>
 				<FlatButton
