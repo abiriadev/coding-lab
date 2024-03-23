7dfed0d39bf8b77d02979f70530d5d5be1572eab
Abiria <abiria.dev@gmail.com>
2024-03-23 20:20:48 +0900

    docs: extract repeated props to global `args`

--- apps/frontend/src/components/VoteButton.stories.ts
+++ apps/frontend/src/components/VoteButton.stories.ts
@@ -8,27 +8,26 @@ const meta = {
 		layout: 'centered',
 	},
 	tags: ['autodocs'],
+	args: {
+		count: 123,
+	},
 } satisfies Meta<typeof VoteButton>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-	args: {
-		count: 123,
-	},
+	args: {},
 }
 
 export const DownVote: Story = {
 	args: {
 		down: true,
-		count: 123,
 	},
 }
 
 export const Pressed: Story = {
 	args: {
 		pressed: true,
-		count: 123,
 	},
 }
