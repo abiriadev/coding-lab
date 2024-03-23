bb4dc654415c50577ad329a52721d71b42df636e
Abiria <abiria.dev@gmail.com>
2024-03-23 20:03:56 +0900

    feat: add `<VoteButton />`

---
+++ apps/frontend/src/components/VoteButton.tsx
@@ -0,0 +1,6 @@
+import { ThumbUpOutlined } from '@mui/icons-material'
+import { RoundButton } from './RoundButton'
+
+export const VoteButton = () => (
+	<RoundButton icon={<ThumbUpOutlined />}>0</RoundButton>
+)
