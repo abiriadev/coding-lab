e375b9546e7d71b904338b8d3a364475c37153b3
Abiria <abiria.dev@gmail.com>
2024-03-24 02:53:31 +0900

    feat: make `<FlatButton />` customizable

--- apps/frontend/src/components/FlatButton.tsx
+++ apps/frontend/src/components/FlatButton.tsx
@@ -2,13 +2,25 @@ import { ReactNode } from 'react'
 
 export interface FlatButtonProps {
 	children?: ReactNode
+	color?: string
+	hover?: string
+	active?: string
 }
 
 export const FlatButton = ({
 	children,
+	color,
+	hover,
+	active,
 }: FlatButtonProps) => {
 	return (
-		<button className="rounded-full border-0 bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 px-5 py-3 font-bold">
+		<button
+			className={`rounded-full border-0 px-5 py-3 font-bold ${
+				color ?? 'bg-neutral-200 '
+			} hover:${hover ?? 'bg-neutral-300 '} active:${
+				active ?? 'bg-neutral-400 '
+			}`}
+		>
 			{children}
 		</button>
 	)
