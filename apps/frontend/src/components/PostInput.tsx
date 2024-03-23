ada6bae3d3c367bbe26566c7d2976fe4dae702c0
Abiria <abiria.dev@gmail.com>
2024-03-24 03:48:58 +0900

    feat: ok this is done

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -1,11 +1,11 @@
-import { TextField, TextareaAutosize } from '@mui/material'
+import { TextareaAutosize } from '@mui/material'
 import { FlatButton } from './FlatButton'
 
 export const PostInput = () => {
 	return (
 		<div className="flex flex-col gap-3">
-			<TextField></TextField>
-			<TextareaAutosize className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-[1.25em] p-5 text-lg" />
+			<input className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg"></input>
+			<TextareaAutosize className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg" />
 			<div className="flex justify-end gap-2">
 				<FlatButton>Cancel</FlatButton>
 				<FlatButton
