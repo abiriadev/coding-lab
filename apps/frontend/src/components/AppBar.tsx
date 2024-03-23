1ab79dec2cbae985c2b6494ba6c378889615984b
Abiria <abiria.dev@gmail.com>
2024-03-24 06:22:31 +0900

    feat: add links

--- apps/frontend/src/components/AppBar.tsx
+++ apps/frontend/src/components/AppBar.tsx
@@ -5,6 +5,8 @@ import {
 	AppBar as MuiAppBar,
 } from '@mui/material'
 import { RoundButton } from './RoundButton'
+import { FaDiscord, FaGithub } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
 
 export interface AppBarProps {
 	position?:
@@ -24,6 +26,26 @@ export const AppBar = ({ position }: AppBarProps) => {
 		>
 			<div className="flex justify-start items-center gap-3">
 				<h1 className="m-0 mr-auto">Coding Lab</h1>
+				<IconContext.Provider
+					value={{
+						color: 'gray',
+						className: 'text-3xl',
+					}}
+				>
+					<a
+						href="https://discord.gg/coding-lab-687271752224735233"
+						className="leading-none mr-3"
+					>
+						<FaDiscord />
+					</a>
+					<a
+						href="https://github.com/abiriadev/coding-lab"
+						className="leading-none"
+					>
+						<FaGithub />
+					</a>
+				</IconContext.Provider>
+
 				<IconButton>
 					<DarkMode fontSize="large" />
 				</IconButton>
