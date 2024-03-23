c33196875a86eaea9c5b26fad514e76b85b81d1a
Abiria <abiria.dev@gmail.com>
2024-03-24 01:56:09 +0900

    feat: add styles

--- apps/frontend/src/components/CommentInput.tsx
+++ apps/frontend/src/components/CommentInput.tsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 export const CommentInput = () => {
 	const [inputMode, setInputMode] = useState(false)
 
-	return inputMode ? (
-		<textarea></textarea>
-	) : (
-		<div onClick={() => setInputMode(true)}>
-			Add new comment...
+	return (
+		<div className="border border-solid rounded-[1.25em] p-3 border-neutral-400 focus-within:border-neutral-800">
+			{inputMode ? (
+				<textarea className="border-0 outline-0 text-sm"></textarea>
+			) : (
+				<div onClick={() => setInputMode(true)}>
+					Add new comment...
+				</div>
+			)}
 		</div>
 	)
 }
