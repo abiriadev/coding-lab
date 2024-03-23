ffbf450b95788ec17378f8997c62bf44b1388fe5
Abiria <abiria.dev@gmail.com>
2024-03-24 03:06:09 +0900

    feat: use ref to auto focus

--- apps/frontend/src/components/CommentInput.tsx
+++ apps/frontend/src/components/CommentInput.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TextareaAutosize } from '@mui/material'
 import { FlatButton } from './FlatButton'
 
 export const CommentInput = () => {
 	const [inputMode, setInputMode] = useState(false)
+	const ta = useRef<null | HTMLTextAreaElement>(null)
+
+	useEffect(() => {
+		if (inputMode) {
+			ta.current?.focus()
+		}
+	}, [inputMode])
 
 	return (
 		<div className="border-2 border-solid rounded-[1.25em] p-2 border-neutral-400 focus-within:border-neutral-800">
 			{inputMode ? (
 				<div className="flex flex-col">
-					<TextareaAutosize className="border-0 outline-0 text-lg mx-3 mt-3 mb-5" />
+					<TextareaAutosize
+						ref={ta}
+						className="border-0 outline-0 text-lg mx-3 mt-3 mb-5"
+					/>
 					<div className="flex justify-end gap-2">
 						<FlatButton
 							onClick={() =>
