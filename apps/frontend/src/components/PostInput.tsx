aab1efa8b74255f60573c1551c901d3ddad9a1d1
Abiria <abiria.dev@gmail.com>
2024-03-24 03:39:04 +0900

    feat: show buttons similar to `<CommentInput />`

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -1,11 +1,22 @@
 import { TextField } from '@mui/material'
+import { FlatButton } from './FlatButton'
 
 export const PostInput = () => {
 	return (
 		<div>
 			<TextField></TextField>
 			<div></div>
-			<div></div>
+			<div className="flex justify-end gap-2">
+				<FlatButton>Cancel</FlatButton>
+				<FlatButton
+					color="text-neutral-100"
+					bg="bg-neutral-800"
+					hover="bg-neutral-800"
+					active="bg-neutral-700"
+				>
+					Submit
+				</FlatButton>
+			</div>
 		</div>
 	)
 }
