17450ab5949246e5cc58fe0d91975dee95c60d88
Abiria <abiria.dev@gmail.com>
2024-03-24 03:41:44 +0900

    feat: style textarea

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -1,11 +1,11 @@
-import { TextField } from '@mui/material'
+import { TextField, TextareaAutosize } from '@mui/material'
 import { FlatButton } from './FlatButton'
 
 export const PostInput = () => {
 	return (
 		<div>
 			<TextField></TextField>
-			<div></div>
+			<TextareaAutosize className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-[1.25em] p-5 text-lg" />
 			<div className="flex justify-end gap-2">
 				<FlatButton>Cancel</FlatButton>
 				<FlatButton
