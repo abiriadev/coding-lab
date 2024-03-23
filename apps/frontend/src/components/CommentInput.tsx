7285e4dcaeac2146cbafa986b281119fad31b7f6
Abiria <abiria.dev@gmail.com>
2024-03-24 02:31:54 +0900

    feat: add flat buttons

--- apps/frontend/src/components/CommentInput.tsx
+++ apps/frontend/src/components/CommentInput.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import { TextareaAutosize } from '@mui/material'
+import { FlatButton } from './FlatButton'
 
 export const CommentInput = () => {
 	const [inputMode, setInputMode] = useState(false)
 
 	return (
-		<div className="border border-solid rounded-[1.25em] p-3 border-neutral-400 focus-within:border-neutral-800">
+		<div className="border-2 border-solid rounded-[1.25em] p-2 border-neutral-400 focus-within:border-neutral-800">
 			{inputMode ? (
-				<TextareaAutosize className="border-0 outline-0 text-sm" />
+				<div className="flex flex-col">
+					<TextareaAutosize className="border-0 outline-0 text-lg mx-3 mt-3 mb-5" />
+					<div className="flex justify-end gap-2">
+						<FlatButton>Cancel</FlatButton>
+						<FlatButton>Submit</FlatButton>
+					</div>
+				</div>
 			) : (
 				<div onClick={() => setInputMode(true)}>
 					Add new comment...
