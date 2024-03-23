c3f906341ff0327a1a16ec84b2925906ab0b11a5
Abiria <abiria.dev@gmail.com>
2024-03-24 07:15:54 +0900

    feat: render `<PostPage />`

--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -1,11 +1,16 @@
 import { Page404 } from './routes/404.tsx'
 import { IndexPage } from './routes/index.tsx'
 import { createBrowserRouter } from 'react-router-dom'
+import { PostPage } from './routes/post.tsx'
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <IndexPage />,
 		errorElement: <Page404 />,
+	},
+	{
+		path: '/posts/:id',
+		element: <PostPage />,
 	},
 ])
