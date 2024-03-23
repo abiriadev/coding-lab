82c420552fe7c3f5cbf8e958136d82a98459d85f
Abiria <abiria.dev@gmail.com>
2024-03-24 02:01:36 +0900

    feat: add `<FlatButton />` component

---
+++ apps/frontend/src/components/FlatButton.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+export interface FlatButtonProps {
+	children: ReactNode
+}
+
+export const FlatButton = ({
+	children,
+}: FlatButtonProps) => {
+	return <button>{children}</button>
+}
