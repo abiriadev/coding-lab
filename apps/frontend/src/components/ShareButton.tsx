326e3d7bd90ab32a894ef86283460fcc4b494093
Abiria <abiria.dev@gmail.com>
2024-03-23 20:37:15 +0900

    feat: add `<ShareButton />`

---
+++ apps/frontend/src/components/ShareButton.tsx
@@ -0,0 +1,8 @@
+import { ShareOutlined } from '@mui/icons-material'
+import { RoundButton } from './RoundButton'
+
+export const ShareButton = () => (
+	<RoundButton icon={<ShareOutlined />}>
+		Share
+	</RoundButton>
+)
