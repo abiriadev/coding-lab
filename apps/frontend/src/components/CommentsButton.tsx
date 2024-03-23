493c599c7b67bc2234e2a436b131d4dc2d5d6358
Abiria <abiria.dev@gmail.com>
2024-03-23 20:24:34 +0900

    feat: extract `count` prop

--- apps/frontend/src/components/CommentsButton.tsx
+++ apps/frontend/src/components/CommentsButton.tsx
@@ -1,8 +1,14 @@
 import { ChatBubbleOutlineOutlined } from '@mui/icons-material'
 import { RoundButton } from './RoundButton'
 
-export const CommentsButton = () => (
+export interface CommentsButtonProps {
+	count?: number
+}
+
+export const CommentsButton = ({
+	count,
+}: CommentsButtonProps) => (
 	<RoundButton icon={<ChatBubbleOutlineOutlined />}>
-		0
+		{count ?? 0}
 	</RoundButton>
 )
