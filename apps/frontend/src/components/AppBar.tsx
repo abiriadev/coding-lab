827fa41d9eef5e9df691dea3ee95925bf859e196
Abiria <abiria.dev@gmail.com>
2024-03-24 06:16:43 +0900

    feat: add buttons

--- apps/frontend/src/components/AppBar.tsx
+++ apps/frontend/src/components/AppBar.tsx
@@ -1,4 +1,10 @@
-import { Avatar, AppBar as MuiAppBar } from '@mui/material'
+import { DarkMode, Add } from '@mui/icons-material'
+import {
+	Avatar,
+	IconButton,
+	AppBar as MuiAppBar,
+} from '@mui/material'
+import { RoundButton } from './RoundButton'
 
 export interface AppBarProps {
 	position?:
@@ -16,9 +22,14 @@ export const AppBar = ({ position }: AppBarProps) => {
 			color="inherit"
 			className="p-4"
 		>
-			<div className="flex justify-start items-center">
+			<div className="flex justify-start items-center gap-3">
 				<h1 className="m-0 mr-auto">Coding Lab</h1>
-
+				<IconButton>
+					<DarkMode fontSize="large" />
+				</IconButton>
+				<RoundButton icon={<Add />}>
+					Post
+				</RoundButton>
 				<Avatar className=""></Avatar>
 			</div>
 		</MuiAppBar>
