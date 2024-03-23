85b50a9c9ef8bcbc8d6e4b2a94cd84195dff92dd
Abiria <abiria.dev@gmail.com>
2024-03-23 20:23:03 +0900

    feat: add `<CommentsButton />`

---
+++ apps/frontend/src/components/CommentsButton.tsx
@@ -0,0 +1,8 @@
+import { ChatBubbleOutlineOutlined } from '@mui/icons-material'
+import { RoundButton } from './RoundButton'
+
+export const CommentsButton = () => (
+	<RoundButton icon={<ChatBubbleOutlineOutlined />}>
+		0
+	</RoundButton>
+)
