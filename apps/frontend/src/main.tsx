6394572cb153ec2e536696e821e0bbca498cf0b3
Abiria <abiria.dev@gmail.com>
2024-03-24 05:13:17 +0900

    build: set alias

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { StyledEngineProvider } from '@mui/material'
 import {
 	createBrowserRouter,
@@ -8,6 +7,7 @@ import {
 } from 'react-router-dom'
 import { Page404 } from './routes/404.tsx'
 import { IndexPage } from './routes/index.tsx'
+import './index.css'
 
 const router = createBrowserRouter([
 	{
