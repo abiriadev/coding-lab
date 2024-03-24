dece58b51ed2f9363364ddff3d4615d73568a256
Abiria <abiria.dev@gmail.com>
2024-03-24 17:10:07 +0900

    feat: implement Promise to suspense logic

---
+++ apps/frontend/src/utils/promise2resource.ts
@@ -0,0 +1,15 @@
+const cache = new Map<() => Promise<any>, [boolean, any]>()
+
+export const promise2resource = <T>(
+	promiseFactory: () => Promise<T>,
+): [boolean, T] => {
+	const cached = cache.get(promiseFactory)
+	if (cached !== undefined) return cached
+
+	const p = promiseFactory()
+	p.then(d => cache.set(promiseFactory, [true, d])).catch(
+		e => cache.set(promiseFactory, [false, e]),
+	)
+
+	throw p
+}
