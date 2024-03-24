b53ada223af0032ecbefce807beea1a2f0372951
Abiria <abiria.dev@gmail.com>
2024-03-24 16:37:06 +0900

    feat: implement `getPosts`

--- apps/frontend/src/api.ts
+++ apps/frontend/src/api.ts
@@ -4,3 +4,7 @@ import PocketBase from 'pocketbase'
 const pb = new PocketBase(
 	'http://127.0.0.1:8090',
 ) as TypedPocketBase
+
+export const getPosts = async () => {
+	return await pb.collection('posts').getFullList()
+}
