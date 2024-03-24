b9e9f8fe28d607bec59987469ef38ea648346fca
Abiria <abiria.dev@gmail.com>
2024-03-24 17:26:34 +0900

    feat: apply style

--- apps/frontend/src/components/ErrorUi.tsx
+++ apps/frontend/src/components/ErrorUi.tsx
@@ -6,9 +6,11 @@ export interface ErrorUiProps {
 
 export const ErrorUi = ({ message }: ErrorUiProps) => {
 	return (
-		<div>
+		<div className="flex flex-col items-center">
 			<Error className="text-red-500 text-[5rem]" />
-			<p>{message ?? 'Error'}</p>
+			<p className="mt-2 mb-0 text-xl font-bold">
+				{message ?? 'Error'}
+			</p>
 		</div>
 	)
 }
