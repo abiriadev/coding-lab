8de464289468268d1360cdf8b9784172bbd66cd9
Abiria <abiria.dev@gmail.com>
2024-03-25 05:16:34 +0900

    feat: add a migration for initial setting

---
+++ apps/backend/migrations/1711308891_settings.go
@@ -0,0 +1,57 @@
+package migrations
+
+import (
+	"errors"
+	"os"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		settings, err := dao.FindSettings()
+		if err != nil {
+			return err
+		}
+
+		if appName := os.Getenv("APP_NAME"); appName == "" {
+			return errors.New("environment variable APP_NAME is required")
+		} else {
+			settings.Meta.AppName = appName
+		}
+
+		if appUrl := os.Getenv("APP_URL"); appUrl == "" {
+			return errors.New("environment variable APP_URL is required")
+		} else {
+			settings.Meta.AppUrl = appUrl
+		}
+
+		settings.DiscordAuth.Enabled = true
+
+		if clientId := os.Getenv("DISCORD_OAUTH_CLIENT_ID"); clientId == "" {
+			return errors.New("environment variable DISCORD_OAUTH_CLIENT_ID is required")
+		} else {
+			settings.DiscordAuth.ClientId = clientId
+		}
+
+		if clientSecret := os.Getenv("DISCORD_OAUTH_CLIENT_SECRET"); clientSecret == "" {
+			return errors.New("environment variable DISCORD_OAUTH_CLIENT_SECRET is required")
+		} else {
+			settings.DiscordAuth.ClientSecret = clientSecret
+		}
+
+		if err := dao.SaveSettings(settings); err != nil {
+			return err
+		}
+
+		return nil
+	}, func(db dbx.Builder) error {
+		// add down queries...
+
+		return nil
+	})
+}
