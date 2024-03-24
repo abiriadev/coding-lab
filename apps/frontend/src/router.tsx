2c0032c986c5b9a8bd8027fd64c679b593b1fcc8
Abiria <abiria.dev@gmail.com>
2024-03-24 18:33:27 +0900

    feat: add a new page `<PostInputPage />`

--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -2,12 +2,17 @@ import { Page404 } from './routes/404.tsx'
 import { IndexPage } from './routes/index.tsx'
 import { createBrowserRouter } from 'react-router-dom'
 import { PostPage } from './routes/post.tsx'
+import { PostInputPage } from './routes/PostInputPage.tsx'
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <IndexPage />,
 		errorElement: <Page404 />,
+	},
+	{
+		path: '/new',
+		element: <PostInputPage />,
 	},
 	{
 		path: '/posts/:id',
---
+++ apps/frontend/src/routes/PostInputPage.tsx
@@ -0,0 +1,11 @@
+import { PostInput } from '@/components/PostInput'
+import { Layout } from './Layout'
+
+export const PostInputPage = () => {
+	return (
+		<Layout>
+			<div className="h-10"></div>
+			<PostInput />
+		</Layout>
+	)
+}
