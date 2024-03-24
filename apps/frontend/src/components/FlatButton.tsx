15e6126c9c43e0281cb539a7905a401eb8f4dcb1
Abiria <abiria.dev@gmail.com>
2024-03-24 18:39:38 +0900

    fix: do not concat classname manually

--- apps/frontend/src/components/FlatButton.tsx
+++ apps/frontend/src/components/FlatButton.tsx
@@ -2,28 +2,18 @@ import { ReactNode } from 'react'
 
 export interface FlatButtonProps {
 	children?: ReactNode
-	color?: string
-	bg?: string
-	hover?: string
-	active?: string
+	className?: string
 	onClick?: () => void
 }
 
 export const FlatButton = ({
 	children,
-	color,
-	bg,
-	hover,
-	active,
 	onClick,
+	className,
 }: FlatButtonProps) => {
 	return (
 		<button
-			className={`rounded-full border-0 px-5 py-3 font-bold ${color ?? 'text-neutral-900'} ${
-				bg ?? 'bg-neutral-200 '
-			} hover:${hover ?? 'bg-neutral-300 '} active:${
-				active ?? 'bg-neutral-400 '
-			}`}
+			className={`rounded-full border-0 px-5 py-3 font-bold text-neutral-900 bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400 ${className}`}
 			onClick={onClick}
 		>
 			{children}
