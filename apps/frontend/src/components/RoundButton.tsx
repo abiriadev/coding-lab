5c8504404f6783c3dac031297a56502f8bd67ac3
Abiria <abiria.dev@gmail.com>
2024-03-24 23:43:10 +0900

    feat: support `onClick` prop for `RoundButton`

--- apps/frontend/src/components/RoundButton.tsx
+++ apps/frontend/src/components/RoundButton.tsx
@@ -2,17 +2,20 @@ import { Button } from '@mui/material'
 import { ReactNode } from 'react'
 
 export interface RoundButtonProps {
-	children?: ReactNode
 	icon?: ReactNode
+	onClick?: () => void
+	children?: ReactNode
 }
 
 export const RoundButton = ({
 	icon,
+	onClick,
 	children,
 }: RoundButtonProps) => (
 	<Button
 		color="inherit"
 		startIcon={icon}
+		onClick={onClick}
 		className="rounded-full bg-neutral-100 px-4"
 	>
 		{children}
