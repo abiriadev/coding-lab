5252d10c495ea2e0a4ca4b5f58fadda1decf7ad9
Abiria <abiria.dev@gmail.com>
2024-03-24 23:04:47 +0900

    feat: use wave animation

--- apps/frontend/src/components/PostSkeleton.tsx
+++ apps/frontend/src/components/PostSkeleton.tsx
@@ -3,12 +3,23 @@ import { Avatar, Skeleton } from '@mui/material'
 export const PostSkeleton = () => (
 	<div className="flex flex-col gap-3">
 		<div className="flex gap-4">
-			<Skeleton variant="circular">
+			<Skeleton variant="circular" animation="wave">
 				<Avatar />
 			</Skeleton>
-			<Skeleton className="text-2xl w-1/4" />
+			<Skeleton
+				className="text-2xl w-1/4"
+				animation="wave"
+			/>
 		</div>
-		<Skeleton className="text-5xl w-4/5" />
-		<Skeleton variant="rounded" height="200px" />
+		<Skeleton
+			className="text-5xl w-4/5"
+			animation="wave"
+		/>
+
+		<Skeleton
+			variant="rounded"
+			height="200px"
+			animation="wave"
+		/>
 	</div>
 )
