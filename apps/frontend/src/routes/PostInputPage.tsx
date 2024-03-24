ea272db5573b48db2a89f04238e080444412120d
Abiria <abiria.dev@gmail.com>
2024-03-24 19:19:51 +0900

    feat: show error

--- apps/frontend/src/routes/PostInputPage.tsx
+++ apps/frontend/src/routes/PostInputPage.tsx
@@ -1,16 +1,32 @@
 import { PostInput } from '@/components/PostInput'
 import { Layout } from './Layout'
 import { createPost } from '@/api'
+import { ErrorUi } from '@/components/ErrorUi'
+import { useState } from 'react'
 
 export const PostInputPage = () => {
+	const [err, setErr] = useState(null)
+	const isErr = err !== null
+
 	return (
 		<Layout>
 			<div className="h-10"></div>
-			<PostInput
-				onSubmit={async d => {
-					await createPost(d)
-				}}
-			/>
+			{isErr ? (
+				<ErrorUi
+					message="Failed to create new post"
+					error={err}
+				/>
+			) : (
+				<PostInput
+					onSubmit={async d => {
+						try {
+							await createPost(d)
+						} catch (e) {
+							setErr(e as any)
+						}
+					}}
+				/>
+			)}
 		</Layout>
 	)
 }
