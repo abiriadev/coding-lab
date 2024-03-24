6e34a88c0aefc34b109cf486a644d97e852dd0ea
Abiria <abiria.dev@gmail.com>
2024-03-24 19:04:22 +0900

    feat: use ref to get data

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -1,6 +1,7 @@
 import { TextareaAutosize } from '@mui/material'
 import { FlatButton } from './FlatButton'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
 
 export interface PostInputSubmitData {
 	title: string
@@ -11,20 +12,37 @@ export interface PostInputProps {
 	onSubmit?: (_: PostInputSubmitData) => void
 }
 
-export const PostInput = () => {
+export const PostInput = ({ onSubmit }: PostInputProps) => {
+	const title = useRef<HTMLInputElement>(null)
+	const content = useRef<HTMLTextAreaElement>(null)
+
 	return (
 		<div className="flex flex-col gap-3">
-			<input className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg"></input>
-			<TextareaAutosize className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg" />
+			<input
+				ref={title}
+				className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg"
+				required
+			></input>
+			<TextareaAutosize
+				ref={content}
+				className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg"
+				required
+			/>
 			<div className="flex justify-end gap-2">
 				<Link to="/">
 					<FlatButton>Cancel</FlatButton>
 				</Link>
 				<FlatButton
 					className="text-white bg-neutral-800 hover:bg-neutral-800 active:bg-neutral-700"
-					onClick={() => {
-						onSubmit()
-					}}
+					onClick={() =>
+						onSubmit?.({
+							title:
+								title.current?.value ?? '',
+							content:
+								content.current?.value ??
+								'',
+						})
+					}
 				>
 					Submit
 				</FlatButton>
