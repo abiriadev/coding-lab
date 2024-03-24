886f54ebb4df17705d8af01dc267472fe65f96d1
Abiria <abiria.dev@gmail.com>
2024-03-24 23:03:07 +0900

    feat: implement Skeleton

--- apps/frontend/src/components/PostSkeleton.tsx
+++ apps/frontend/src/components/PostSkeleton.tsx
@@ -1,1 +1,14 @@
-export const PostSkeleton = () => <></>
+import { Avatar, Skeleton } from '@mui/material'
+
+export const PostSkeleton = () => (
+	<div className="flex flex-col gap-3">
+		<div className="flex gap-4">
+			<Skeleton variant="circular">
+				<Avatar />
+			</Skeleton>
+			<Skeleton className="text-2xl w-1/4" />
+		</div>
+		<Skeleton className="text-5xl w-4/5" />
+		<Skeleton variant="rounded" height="200px" />
+	</div>
+)
