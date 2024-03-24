5b5af046a9a596de2cc94c4a1f688a1707b529fb
Abiria <abiria.dev@gmail.com>
2024-03-24 17:23:50 +0900

    feat: show message

--- apps/frontend/src/components/ErrorUi.tsx
+++ apps/frontend/src/components/ErrorUi.tsx
@@ -1,5 +1,14 @@
+import { Error } from '@mui/icons-material'
+
 export interface ErrorUiProps {
-	message: string
+	message?: string
 }
 
-export const ErrorUi = () => {}
+export const ErrorUi = ({ message }: ErrorUiProps) => {
+	return (
+		<div>
+			<Error className="text-red-500 text-[5rem]" />
+			<p>{message ?? 'Error'}</p>
+		</div>
+	)
+}
