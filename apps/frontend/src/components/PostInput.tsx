88d96f25dfd3886309fa9a6cb6c87824f4250205
Abiria <abiria.dev@gmail.com>
2024-03-24 18:46:10 +0900

    feat: add link to main

--- apps/frontend/src/components/PostInput.tsx
+++ apps/frontend/src/components/PostInput.tsx
@@ -1,5 +1,6 @@
 import { TextareaAutosize } from '@mui/material'
 import { FlatButton } from './FlatButton'
+import { Link } from 'react-router-dom'
 
 export const PostInput = () => {
 	return (
@@ -7,7 +8,9 @@ export const PostInput = () => {
 			<input className="border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg"></input>
 			<TextareaAutosize className="min-h-40 border-2 border-solid border-neutral-400 focus-within:border-neutral-800 rounded-xl p-3 text-lg" />
 			<div className="flex justify-end gap-2">
-				<FlatButton>Cancel</FlatButton>
+				<Link to="/">
+					<FlatButton>Cancel</FlatButton>
+				</Link>
 				<FlatButton className="text-white bg-neutral-800 hover:bg-neutral-800 active:bg-neutral-700">
 					Submit
 				</FlatButton>
