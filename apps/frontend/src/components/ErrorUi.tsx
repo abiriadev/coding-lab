5eaa710dcdf2971341de458c324c9fbd8491bb73
Abiria <abiria.dev@gmail.com>
2024-03-24 17:29:56 +0900

    feat: show details

--- apps/frontend/src/components/ErrorUi.tsx
+++ apps/frontend/src/components/ErrorUi.tsx
@@ -2,15 +2,20 @@ import { Error } from '@mui/icons-material'
 
 export interface ErrorUiProps {
 	message?: string
+	error?: unknown
 }
 
-export const ErrorUi = ({ message }: ErrorUiProps) => {
+export const ErrorUi = ({
+	message,
+	error,
+}: ErrorUiProps) => {
 	return (
 		<div className="flex flex-col items-center">
 			<Error className="text-red-500 text-[5rem]" />
 			<p className="mt-2 mb-0 text-xl font-bold">
 				{message ?? 'Error'}
 			</p>
+			{error ? <p>{error.toString()}</p> : null}
 		</div>
 	)
 }
