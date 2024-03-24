8a756fc8a3527e6a62bba660bf1b0f1221968dec
Abiria <abiria.dev@gmail.com>
2024-03-25 00:00:46 +0900

    feat: show Snackbar

--- apps/frontend/src/components/ShareButton.tsx
+++ apps/frontend/src/components/ShareButton.tsx
@@ -1,23 +1,37 @@
 import { ShareOutlined } from '@mui/icons-material'
 import { RoundButton } from './RoundButton'
+import { Snackbar } from '@mui/material'
+import { useState } from 'react'
 
 export interface ShareButtonProps {
 	id?: string
 }
 
-export const ShareButton = ({ id }: ShareButtonProps) => (
-	<RoundButton
-		icon={<ShareOutlined />}
-		onClick={async () => {
-			if (!id) return
+export const ShareButton = ({ id }: ShareButtonProps) => {
+	const [show, setShow] = useState(false)
 
-			try {
-				await navigator.clipboard.writeText(
-					`${import.meta.env.VITE_BASE_URL}/posts/${id}`,
-				)
-			} catch {}
-		}}
-	>
-		Share
-	</RoundButton>
-)
+	return (
+		<RoundButton
+			icon={<ShareOutlined />}
+			onClick={async () => {
+				if (!id) return
+
+				try {
+					await navigator.clipboard.writeText(
+						`${import.meta.env.VITE_BASE_URL}/posts/${id}`,
+					)
+
+					setShow(true)
+				} catch {}
+			}}
+		>
+			Share
+			<Snackbar
+				open={show}
+				autoHideDuration={500}
+				onClose={() => setShow(false)}
+				message="Link copied!"
+			/>
+		</RoundButton>
+	)
+}
