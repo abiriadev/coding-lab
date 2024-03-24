1504dab828353ce2511b974b62fd19384db947c8
Abiria <abiria.dev@gmail.com>
2024-03-24 22:40:23 +0900

    feat: fetch data using suspense

---
+++ apps/frontend/src/routes/PostPage.tsx
@@ -0,0 +1,42 @@
+import { Post } from '@components/Post'
+import { useParams } from 'react-router-dom'
+import { Layout } from './Layout'
+import { Suspense } from 'react'
+import { promise2resource } from '@/utils/promise2resource'
+import { getPost } from '@/api'
+import { ErrorUi } from '@components/ErrorUi'
+
+let cache: Record<string, () => Promise<any>> = {}
+
+export const PostPage = () => {
+	const { id } = useParams()
+	if (id === undefined)
+		throw new Error('id was not defined')
+	if (!cache[id]) cache[id] = () => getPost(id)
+
+	return (
+		<Layout>
+			<div className="h-10"></div>
+			<Suspense fallback="loading">
+				<PostInner id={id} />
+			</Suspense>
+		</Layout>
+	)
+}
+
+const PostInner = ({ id }: { id: string }) => {
+	const [ok, post] = promise2resource(cache[id])
+
+	return ok ? (
+		<Post
+			title={post.title}
+			content={post.content}
+			comments={[]}
+		/>
+	) : (
+		<ErrorUi
+			message="Failed to fetch post"
+			error={post}
+		/>
+	)
+}
