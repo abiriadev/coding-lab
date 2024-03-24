b876d064d29ee9b3a2eb83a2d24e1dc024cbd3f3
Abiria <abiria.dev@gmail.com>
2024-03-24 22:43:05 +0900

    docs: add a story for `<PostSkeleton />`

---
+++ apps/frontend/src/components/PostSkeleton.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { PostSkeleton } from './PostSkeleton'
+
+const meta = {
+	title: 'Example/PostSkeleton',
+	component: PostSkeleton,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof PostSkeleton>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+	args: {},
+}
