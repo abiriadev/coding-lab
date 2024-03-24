e84f0b124daffbde90235c47afa4e0ae6f3dbf38
Abiria <abiria.dev@gmail.com>
2024-03-25 03:10:38 +0900

    feat: set avatar for new users automatically

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/forms"
 )
 
 func main() {
@@ -16,6 +17,25 @@ func main() {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
 	})
+
+	app.OnRecordAfterAuthWithOAuth2Request("users").
+		Add(func(e *core.RecordAuthWithOAuth2Event) error {
+			if e.ProviderName != "discord" || !e.IsNewRecord {
+				return nil
+			}
+
+			form := forms.NewRecordUpsert(app, e.Record)
+
+			form.LoadData(map[string]any{
+				"avatar": e.OAuth2User.AvatarUrl,
+			})
+
+			if err := form.Submit(); err != nil {
+				return err
+			}
+
+			return nil
+		})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
