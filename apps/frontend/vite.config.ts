e0bc486ca9aab8d83fcc27be2bf08954bc5ac972
Abiria <abiria.dev@gmail.com>
2024-03-26 22:28:59 +0900

    build: support GA

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'),
-			'@components': resolve(
-				__dirname,
-				'./src/components',
-			),
-			'@routes': resolve(__dirname, './src/routes'),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return {
+		plugins: [
+			react(),
+			VitePluginRadar({
+				analytics: {
+					id: env.GA_MEASUREMENT_ID,
+				},
+			}),
+		],
+
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src'),
+				'@components': resolve(
+					__dirname,
+					'./src/components',
+				),
+				'@routes': resolve(
+					__dirname,
+					'./src/routes',
+				),
+			},
 		},
-	},
+	}
 })
